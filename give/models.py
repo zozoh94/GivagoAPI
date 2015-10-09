17bcc49695ea29f9b6925c62b63b83bbf5ab53ab
Enzo Hamelin <enzo.hamelin@gmail.com>
2015-10-09 16:23:32 +0200

    ONG model

--- give/models.py
+++ give/models.py
@@ -3,4 +3,10 @@ from django.db import models
 class Gift(models.Model):
     name = models.CharField(max_length=255, unique=True)
     icon = models.CharField(max_length=255, null=True)
-    credits = models.IntegerField(default=0)
+    ong = models.ForeignKey('ONG', related_name='gifts', on_delete=models.SET_NULL, null=True, blank=True)
+
+class ONG(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+    logo = models.ImageField(upload_to='ong_logo', null=True, blank=True)
+    site = models.URLField(null=True, blank=True)
--- give/views.py
+++ give/views.py
@@ -5,6 +5,6 @@ from .serializers import GiftSerializer
 from .models import Gift
 
 class GiftViewSet(viewsets.ModelViewSet):
-    queryset = Gift.objects.all()
+    queryset = Gift.objects.filter(ong__isnull=False)
     serializer_class = GiftSerializer
     permission_classes = (permissions.DjangoModelPermissionsOrAnonReadOnly, )
