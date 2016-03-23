95fead3fc11eb2c7b2b05af478b31e0b7dee39bf
Enzo Hamelin <enzo.hamelin@gmail.com>
2016-03-23 17:47:06 +0100

    On enregistre pas l'user por l'instant

--- actfund/views.py
+++ actfund/views.py
@@ -52,14 +52,14 @@ def survey_completed_view(request):
 
     survey = Survey()    
     
-    user = None
+    """user = None
     if(user_id and user_id != 0 and user_id != 1):
         try:
             user = get_user_model().objects.get(id=user_id)
         except get_user_model().DoesNotExist:
             return  HttpResponseServerError(user_id)
             raise Http404("User does not exist")
-    survey.user = user
+    survey.user = user"""
         
     if(amount):
         try:            
